7808a9fc22a21599f393119549d761c1e34f2a4d
RobertoScarci <robertoscarci.02@gmail.com>
2026-01-21 13:05:46 +0100

    style: align footer social links to left

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-40">
       <div className="container mx-auto px-6 py-6">
-          <div className="max-w-4xl mx-auto">
+        <div className="max-w-4xl">
           <div className="flex items-center gap-6">
             {socialLinks.map((social) => {
               const Icon = social.icon;
